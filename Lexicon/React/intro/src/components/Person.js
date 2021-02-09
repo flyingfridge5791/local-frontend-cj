cf030bd2a062f139d5231fd43aa477f60c667103
Cliron <crippen91@hotmail.com>
2021-02-09 11:16:37 +0100

    no message

---
+++ Lexicon/React/intro/src/components/Person.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Person() {
+     return (
+          <div className="border">
+               <h1>Name: Chrippe</h1>
+               <p>Age: 33</p>
+
+          </div>
+     )
+}
+
